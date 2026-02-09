84cfe4c98927c31cc5ce84b9c88f76965fe1caf8
Carl <carl.waerner@hyperisland.se>
2026-02-10 00:52:07 +0100

    Add missing Sanity queries for posts and sitemap

--- frontend/sanity/lib/queries.ts
+++ frontend/sanity/lib/queries.ts
@@ -77,6 +77,67 @@ export const pagesSlugs = defineQuery(`
   }
 `)
 
+export const postPagesSlugs = defineQuery(`
+  *[_type == "post" && defined(slug.current)]{
+    "slug": slug.current
+  }
+`)
+
+export const allPostsQuery = defineQuery(`
+  *[_type == "post"] | order(date desc) {
+    _id,
+    title,
+    "slug": slug.current,
+    excerpt,
+    date,
+    "author": author->{
+      firstName,
+      lastName,
+      picture
+    }
+  }
+`)
+
+export const morePostsQuery = defineQuery(`
+  *[_type == "post" && _id != $skip] | order(date desc) [0...$limit] {
+    _id,
+    title,
+    "slug": slug.current,
+    excerpt,
+    date,
+    "author": author->{
+      firstName,
+      lastName,
+      picture
+    }
+  }
+`)
+
+export const postQuery = defineQuery(`
+  *[_type == "post" && slug.current == $slug][0]{
+    _id,
+    title,
+    "slug": slug.current,
+    excerpt,
+    date,
+    content,
+    coverImage,
+    "author": author->{
+      firstName,
+      lastName,
+      picture
+    }
+  }
+`)
+
+export const sitemapData = defineQuery(`
+  *[_type in ["page", "post"] && defined(slug.current)]{
+    _type,
+    "slug": slug.current,
+    _updatedAt
+  }
+`)
+
 export const servicesPageQuery = defineQuery(`
   *[_type == "servicesPage" && _id == "servicesPage"][0]{
     _id,
